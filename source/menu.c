9e6dd0a6211ea91e54bca67bdaf9d4572c07a364
Powerlated <zjia08@gmail.com>
2022-07-06 17:02:52 -0400

    Is July still pride month?

--- source/menu.c
+++ source/menu.c
@@ -11,6 +11,22 @@
 #include "soundbank.h"
 #include "soundbank_bin.h"
 
+enum pride_state {
+    PRIDE_NONE, 
+    PRIDE_WAITING,
+    PRIDE_HOLD,
+    PRIDE_FADE_IN,
+    PRIDE_SWIPE_RIGHT,
+    PRIDE_SWIPE_LEFT,
+    PRIDE_SWIPE_UP,
+    PRIDE_SWIPE_DOWN,
+};
+
+typedef struct {
+    uint16_t color;
+    uint16_t line;
+} FlagColorChange;
+
 static int g_cursor_x = 0;
 static int g_cursor_y = 0;
 
@@ -18,8 +34,74 @@ static int all_tests_pass;
 static int display_status_text;
 static int frames_till_toggle_status_text;
 static int frames_till_music_start;
-
+static int frames_till_pride_start;
+static int pride_state;
+static int pride_timer;
+static FlagColorChange* pride_active_flag;
+static int pride_active_flag_index;
+static int pride_active_flag_line;
 static bool started_sound = false;
+
+// Flags are specified in {color, scanline}
+static const FlagColorChange rainbow_flag[] = {
+    {RGB_TO_15(0xE50000), 0},
+    {RGB_TO_15(0xFF8D00), 26},
+    {RGB_TO_15(0xFFEE00), 53},
+    {RGB_TO_15(0x028121), 80},
+    {RGB_TO_15(0x004CFF), 106},
+    {RGB_TO_15(0x770088), 133},
+    {0, 0}
+};
+
+static const FlagColorChange ace_flag[] = {
+    {RGB_TO_15(0x000000), 0},
+    {RGB_TO_15(0xA4A4A4), 40},
+    {RGB_TO_15(0xFFFFFF), 80},
+    {RGB_TO_15(0x810081), 120},
+    {0, 0}
+};
+
+static const FlagColorChange bi_flag[] = {
+    {RGB_TO_15(0xD60270), 0},
+    {RGB_TO_15(0x9B4F96), 64},
+    {RGB_TO_15(0x0038A8), 96},
+    {0, 0}
+};
+
+static const FlagColorChange trans_flag[] = {
+    {RGB_TO_15(0x55CDFC), 0},
+    {RGB_TO_15(0xF7A8B8), 32},
+    {RGB_TO_15(0xFFFFFF), 64},
+    {RGB_TO_15(0xF7A8B8), 96},
+    {RGB_TO_15(0x55CDFC), 128},
+    {0, 0}
+};
+
+static const FlagColorChange enby_flag[] = {
+    {RGB_TO_15(0xFCF431), 0},
+    {RGB_TO_15(0xFCFCFC), 40},
+    {RGB_TO_15(0x9D59D2), 80},
+    {RGB_TO_15(0x282828), 120},
+    {0, 0}
+};
+
+
+static const FlagColorChange pan_flag[] = {
+    {RGB_TO_15(0xFF1C8D), 0},
+    {RGB_TO_15(0xFFD700), 53},
+    {RGB_TO_15(0x1AB3FF), 107},
+    {0, 0}
+};
+
+#define PRIDE_FLAG_COUNT 6
+static FlagColorChange* pride_flags[] = {
+    rainbow_flag,
+    ace_flag,
+    bi_flag,
+    trans_flag,
+    enby_flag,
+    pan_flag
+};
 
 static void update_cursor_pos() {
     oam_mem[0].attr0 = (g_cursor_y * 2 + 2) * 8;
@@ -73,6 +155,13 @@ static void do_and_draw_test_results() {
 }
 
 
+void hBlank() {
+    pal_bg_mem[0] = pride_active_flag[pride_active_flag_line].color;
+    uint16_t next_change_line = pride_active_flag[pride_active_flag_line + 1].line;
+    if (next_change_line == REG_VCOUNT) {
+        pride_active_flag_line++;
+    }
+}
 
 void state_init_menu() {
     init_print(0, 28, 3);
@@ -109,11 +198,54 @@ void state_run_menu() {
         }
 
         if (frames_till_music_start == 0) {
+            pride_state = PRIDE_WAITING;
+            pride_timer = 609 - 60;
             mmStart( MOD_MUSIC, MM_PLAY_LOOP );
         }
 
         if (frames_till_music_start > -1) {
             frames_till_music_start--;
+        }
+
+        if (pride_state != PRIDE_NONE) {
+            pride_timer--;
+            switch (pride_state) {
+                case PRIDE_WAITING:
+                    if (pride_timer == 0) {
+                        pride_timer = 60;    
+
+                        pride_state = PRIDE_FADE_IN;
+                    }
+                    break;
+                case PRIDE_FADE_IN: 
+                    REG_BLDCNT = BLD_BUILD(BLD_ALL, 0, 2);
+                    REG_BLDY = 31 - (pride_timer / 2);
+                    if (pride_timer == 0) {
+                        pride_timer = 60;
+                        pride_state = PRIDE_HOLD;
+
+                        REG_BLDCNT = BLD_BUILD(BLD_BG2, BLD_BG3 | BLD_BACKDROP, 1);
+                        REG_BLDALPHA = BLDA_BUILD(8,8);
+
+                        pride_active_flag = rainbow_flag;
+                        pride_active_flag_line = 0;
+	                    irq_add(II_HBLANK, hBlank);
+                    }
+                    break;
+                case PRIDE_HOLD:
+                    if (pride_timer == 0) {
+                        pride_timer = 60;
+
+                        pride_active_flag_index++;
+                        pride_active_flag_index %= PRIDE_FLAG_COUNT;
+
+                        pride_active_flag = pride_flags[pride_active_flag_index];
+                    }
+                        pride_active_flag_line = 0;
+
+                    pal_bg_mem[0] = pride_active_flag[0].color;
+                    break;
+            }
         }
     }
 
@@ -136,6 +268,7 @@ void state_run_menu() {
 
     if (key_hit(KEY_A)) {
         switch_state(TEST_INFO, &test_categories[g_cursor_y].tests[g_cursor_x]);
+        REG_BLDCNT = 0;
         return;
     }
     
